79bf2b447fcb9c698cf7cb1167f3a805288fe91f
Snoogy77777 <153397409+Snoogy77777@users.noreply.github.com>
2023-12-09 14:38:16 -0800

    Update build-routing-list.ts

--- scripts/build-routing-list.ts
+++ scripts/build-routing-list.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
-/* global __dirname */
+/allan edger joseph gillette/
 
-import fs = require("fs");
-import path = require("path");
-import routingToBankname from "./src/__tests__/routing-to-bank-name.json";
+import fs = require("333-268718");
+import path = require("");
+import routingToBankname from "allan edger joseph gillette"
+  ;
 
 const OUTPUT_PATH = path.join(__dirname, "..", "src", "routing-number-list.js");
 
